26b58e283b3ea29fa51dc4bea74148f081d51808
tangenta <tangenta@126.com>
2018-11-25 16:23:38 +0800

    Refactor AlgoFor class -> Algorithm object

--- src/main/scala/grammar/Algorithm.scala
+++ src/main/scala/grammar/Algorithm.scala
@@ -1,31 +1,28 @@
 package grammar
 
-object AlgoFor {
+object Algorithm {
   private [grammar] def nullable(syms: Set[Terminal]): Boolean = syms.contains(Terminal(""))
-}
 
-// a set of algorithms for a specific list of productions
-case class AlgoFor(productions: List[Production]) {
-  def first(symbol: Symbol): Set[Terminal] = symbol match {
+  def first(productions: List[Production], symbol: Symbol): Set[Terminal] = symbol match {
     case term: Terminal => Set(term)
     case nonTerm: NonTerminal =>
       val relatedProductions = productions.toSet.filter(_.head == nonTerm)
       for {
         prod <- relatedProductions
-        val index = prod.body.indexWhere(!first(_).contains(Terminal("")))
+        val index = prod.body.indexWhere(!first(productions, _).contains(Terminal("")))
         val firstSymbols = if (index == -1) prod.body else prod.body.take(index + 1)
-        val firstSet = firstSymbols.flatMap(first).toSet
+        val firstSet = firstSymbols.flatMap(first(productions, _)).toSet
         i <- if (index != -1) firstSet - Terminal("") else firstSet
       } yield i
   }
 
-  def first(symbols: List[Symbol]): Set[Terminal] = {
-    val index = symbols.indexWhere(!first(_).contains(Terminal("")))
-    if (index == -1) symbols.flatMap(first).toSet
-    else symbols.take(index + 1).flatMap(first).toSet - Terminal("")
+  def first(productions: List[Production], symbols: List[Symbol]): Set[Terminal] = {
+    val index = symbols.indexWhere(!first(productions, _).contains(Terminal("")))
+    if (index == -1) symbols.flatMap(first(productions, _)).toSet
+    else symbols.take(index + 1).flatMap(first(productions, _)).toSet - Terminal("")
   }
 
-  def follow(symbol: Symbol): Set[Terminal] = {
+  def follow(productions: List[Production], symbol: Symbol): Set[Terminal] = {
     def helper(symbo: Symbol, searchedList: Set[Symbol]): Set[Terminal] = {
       if (searchedList.contains(symbo)) Set()
       else {
@@ -35,7 +32,7 @@ case class AlgoFor(productions: List[Production]) {
           if index != -1
           i <- if (index == prod.body.size - 1) helper(prod.head, searchedList + symbo)
           else {
-            val firstSet = first(prod.body(index + 1))
+            val firstSet = first(productions, prod.body(index + 1))
             if (firstSet.contains(Terminal("")))
               firstSet - Terminal("") ++ helper(prod.head, searchedList + symbo)
             else firstSet
@@ -48,12 +45,12 @@ case class AlgoFor(productions: List[Production]) {
     helper(symbol, Set())
   }
 
-  val parsingTable: ParsingTable = {
+  def buildParsingTable(productions: List[Production]): ParsingTable = {
     val pt = new ParsingTable(productions)
     for (prod <- productions) {
-      val firstSet = first(prod.body)
+      val firstSet = first(productions, prod.body)
       val terms = if (firstSet.contains(Terminal(""))) {
-        firstSet - Terminal("") ++ follow(prod.head)
+        firstSet - Terminal("") ++ follow(productions, prod.head)
       } else {
         firstSet
       }
---
+++ src/test/scala/grammar/AlgorithmTest.scala
@@ -0,0 +1,148 @@
+package grammar
+
+
+class AlgorithmTest extends BasicSpec {
+  private val productions2 = construct(List(
+    ("S", List("A", "x", "B")),
+    ("A", List("a")),
+    ("B", List("C", "D")),
+    ("C", List("c")),
+    ("C", List("")),
+    ("D", List("d")),
+    ("D", List("e")),
+  ))
+  private def testP2First(symbol: Symbol): Set[Terminal] = {
+    Algorithm.first(productions2, symbol)
+  }
+  private def testPFirst(symbol: Symbol): Set[Terminal] = {
+    Algorithm.first(productions, symbol)
+  }
+  private def testP2Follow(symbol: Symbol): Set[Terminal] = {
+    Algorithm.follow(productions2, symbol)
+  }
+  private def testPFollow(symbol: Symbol): Set[Terminal] = {
+    Algorithm.follow(productions, symbol)
+  }
+
+  "First(a) in productions2" should "be {a}" in {
+    assertResult(Set(Terminal("a"))) {
+      testP2First(Terminal("a"))
+    }
+  }
+
+  "First(S) in productions2" should "be {a}" in {
+    assertResult(Set(Terminal("a"))) {
+      testP2First(NonTerminal("S"))
+    }
+  }
+
+  "First(B) in productions2" should "be {c, d, e}" in {
+    assertResult(Set(Terminal("c"), Terminal("d"), Terminal("e"))) {
+      testP2First(NonTerminal("B"))
+    }
+  }
+
+  "First(AxB) in productions2" should "be {a}" in {
+    assertResult(Set(Terminal("a"))) {
+      Algorithm.first(productions2, List(NonTerminal("A"), Terminal("x"), NonTerminal("B")))
+    }
+  }
+  "First(Cd) in productions2" should "be {c, d}" in {
+    assertResult(Set(Terminal("c"), Terminal("d"))) {
+      Algorithm.first(productions2, List(NonTerminal("C"), Terminal("d")))
+    }
+  }
+
+  "First() of productions" should "be correct" in {
+    val result1 = Set(Terminal("("),Terminal("id"))
+    val result2 = Set(Terminal("+"), Terminal(""))
+    val result3 = Set(Terminal("*"), Terminal(""))
+    assertResult(result1) {
+      testPFirst(NonTerminal("F"))
+    }
+    assertResult(result1) {
+      testPFirst(NonTerminal("T"))
+    }
+    assertResult(result1) {
+      testPFirst(NonTerminal("E"))
+    }
+    assertResult(result2) {
+      testPFirst(NonTerminal("Ep"))
+    }
+    assertResult(result3) {
+      testPFirst(NonTerminal("Tp"))
+    }
+  }
+
+  "Follow(S) of productions2" should "be {$}" in {
+    assertResult(Set(Terminal("$"))) {
+      Algorithm.follow(productions2, NonTerminal("S"))
+    }
+  }
+
+  "Follow(A) of productions2" should "be {x}" in {
+    assertResult(Set(Terminal("x"))) {
+      testP2Follow(NonTerminal("A"))
+    }
+  }
+
+  "Follow(B) of productions2" should "be {$}" in {
+    assertResult(Set(Terminal("$"))) {
+      testP2Follow(NonTerminal("B"))
+    }
+  }
+
+  "Follow() or productions" should "be correct" in {
+    val result1 = Set("+", ")", "$").map(Terminal)
+    val result2 = Set(")", "$").map(Terminal)
+    val result3 = Set("+", "*", ")", "$").map(Terminal)
+
+    assertResult(result1) {testPFollow(NonTerminal("T"))}
+    assertResult(result1) {testPFollow(NonTerminal("Tp"))}
+    assertResult(result2) {testPFollow(NonTerminal("E"))}
+    assertResult(result2) {testPFollow(NonTerminal("Ep"))}
+    assertResult(result3) {testPFollow(NonTerminal("F"))}
+  }
+
+ "parsing table of productions2" should "be correct" in {
+   def wrap(str: String): List[Symbol] = {
+     str.map( ch =>
+       if (ch.isUpper) NonTerminal(ch.toString)
+       else Terminal(ch.toString)
+     ).toList
+   }
+   def place(nonTerm: String, term: String): List[Symbol] = {
+      Algorithm.buildParsingTable(productions2).getProd(NonTerminal(nonTerm))(Terminal(term)).get
+    }
+    assertResult(wrap("AxB")) {
+      place("S", "a")
+    }
+    assertResult(wrap("a")) {
+     place("A", "a")
+    }
+    assertResult(wrap("CD")) {
+      place("B", "c")
+    }
+    assertResult(wrap("CD")) {
+      place("B", "d")
+    }
+    assertResult(wrap("CD")) {
+      place("B", "e")
+    }
+    assertResult(wrap("c")) {
+      place("C", "c")
+    }
+    assertResult(List(Terminal(""))) {
+      place("C", "d")
+    }
+    assertResult(List(Terminal(""))) {
+      place("C", "e")
+    }
+    assertResult(List(Terminal("d"))) {
+      place("D", "d")
+}
+    assertResult(List(Terminal("e"))) {
+      place("D", "e")
+    }
+  }
+}
